94e4b97a64f65020894d6d5e14cdd0b7a449da31
Rubén Alvarado <92123464+kirake-a@users.noreply.github.com>
2023-03-20 13:07:54 -0600

    Song class

---
+++ ADA06_ImpOrdenamiento/src/model/Song.java
@@ -0,0 +1,155 @@
+package model;
+
+public class Song {
+    private String id;
+    private String name;
+    private double duration;
+    private double energy;
+    private int key;
+    private double loudness;
+    private int mode;
+    private double speechiness;
+    private double acousticness;
+    private double instrumentalness;
+    private double liveness;
+    private double valance;
+    private double tempo;
+    private double danceability;
+
+    public Song() {
+    }
+
+    
+
+    public Song(String id, String name, double duration, double energy, int key, double loudness, int mode,
+            double speechiness, double acousticness, double instrumentalness, double liveness, double valance,
+            double tempo, double danceability) {
+        this.id = id;
+        this.name = name;
+        this.duration = duration;
+        this.energy = energy;
+        this.key = key;
+        this.loudness = loudness;
+        this.mode = mode;
+        this.speechiness = speechiness;
+        this.acousticness = acousticness;
+        this.instrumentalness = instrumentalness;
+        this.liveness = liveness;
+        this.valance = valance;
+        this.tempo = tempo;
+        this.danceability = danceability;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public double getLoudness() {
+        return loudness;
+    }
+
+    public void setLoudness(double loudness) {
+        this.loudness = loudness;
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+
+    public double getSpeechiness() {
+        return speechiness;
+    }
+
+    public void setSpeechiness(double speechiness) {
+        this.speechiness = speechiness;
+    }
+
+    public double getAcousticness() {
+        return acousticness;
+    }
+
+    public void setAcousticness(double acousticness) {
+        this.acousticness = acousticness;
+    }
+
+    public double getInstrumentalness() {
+        return instrumentalness;
+    }
+
+    public void setInstrumentalness(double instrumentalness) {
+        this.instrumentalness = instrumentalness;
+    }
+
+    public double getLiveness() {
+        return liveness;
+    }
+
+    public void setLiveness(double liveness) {
+        this.liveness = liveness;
+    }
+
+    public double getValance() {
+        return valance;
+    }
+
+    public void setValance(double valance) {
+        this.valance = valance;
+    }
+
+    public double getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(double tempo) {
+        this.tempo = tempo;
+    }
+
+    public double getDanceability() {
+        return danceability;
+    }
+
+    public void setDanceability(double danceability) {
+        this.danceability = danceability;
+    }
+
+}
